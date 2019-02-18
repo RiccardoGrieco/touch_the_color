b6e4efc55580151fc52c2cf3c7c02a3c809a0018
Marco Matarese <marco.matarese@studenti.unina.it>
2019-02-18 16:00:48 +0100

    Added subscriber and publisher for RoleManager. Added some attributes.

--- src/Kid.py
+++ src/Kid.py
@@ -2,51 +2,94 @@
 
 import rospy
 
+
 class Kid:
     def __init__(self):
-		rospy.init_node("kid", anonymous=True)
+        rospy.init_node("Kid", anonymous=True)
 
-    def look(self, msg):
+        self.gameStarted = False
+        self.colorToTouch = ""
+
+        # TODO gestire queste bestiole qui
+        self.RMSpeakerPub = rospy.Publisher("node_to_rolemanager", String, queue_size=10)
+        self.RMListenerSub = rospy.Subscriber("rolemanager_to_node", String, self.ownRoleManagerListener)
+
+        self.start()
+
+    def ownRoleManagerListener(self, msg):
+        # manage messages FROM RoleManager reading from its subscriber
+
+        # 0:color (color to touch)
+        # 1 (go!)
+
+        if msg[0] == "0":
+            self.colorToTouch = msg[2:]
+
+        elif msg[0] == "1":
+            i = 0
+            self.gameStarted = True
+
+    def ownRoleManagerSpeaker(self, typeOfMess):
+        # manage messages TO RoleManager writing on its publisher
+
+        # 0 (color touched)
+
+        if typeOfMess == 0:
+            self.RMSpeakerPub.publish("0")      # color touched
+
+    def start(self):
+        rate_start = rospy.Rate(1)
+        rate_start.sleep()
+
+        while not(rospy.is_shutdown()):
+            # print("hey")
+            i = 1
+
+#    def look(self, msg):
         #PS
         #TODO look perception scheme
         # if time elapsed then ...
 
-    def moveToColor(self): #TODO params
+#    def moveToColor(self): #TODO params
         #B
         #TODO move to color behaviour
 
-    def wander(self): #TODO params
+#    def wander(self): #TODO params
         #B
         #TODO wander behaviour
 
-    def avoid(self): #TODO params
+#    def avoid(self): #TODO params
         #B
         #TODO avoid obstacle behaviour
 
-    def move(self): #TODO params
+#    def move(self): #TODO params
         #MS
         #TODO move motor schema
 
-    def integrate(self): #TODO params
+#    def integrate(self): #TODO params
         #B
         #TODO manage objects position after movement
 
-    def feelForce(self): #TODO params
+#    def feelForce(self): #TODO params
         #PS
         #TODO calculate overall external force on robot
     
-    def readSensors(self, msg):
+#    def readSensors(self, msg):
         #PS
         #TODO read ultrasonic sensor information
     
-    def listen(self, msg):
+#    def listen(self, msg):
         #PS
         #TODO listen for messages
 
-    def determineEndGame(self): #TODO params
+#    def determineEndGame(self): #TODO params
         #B
         #TODO determine end game behaviour
 
-    def communicateColorTouched(self): #TODO params
+#   def communicateColorTouched(self): #TODO params
         #MS
-        #TODO communicate that the color has been touched 
+        #TODO communicate that the color has been touched
+
+
+if __name__ == "__main__":
+    rb = Kid()
--- src/Witch.py
+++ src/Witch.py
@@ -4,22 +4,61 @@ import rospy
 
 class Witch:
     def __init__(self):
-        rospy.init_node("witch", anonymous=True)
-        
+        rospy.init_node("Witch", anonymous=True)
+
+        self.noWinners = 0  # when it's noTotKids-1 than the game ends
+        self.noTotKids = 0
+
+        # TODO gestire queste bestiole qui
+        self.RMSpeakerPub = rospy.Publisher("node_to_rolemanager", String, queue_size=10)
+        self.RMListenerSub = rospy.Subscriber("rolemanager_to_node", String, self.ownRoleManagerListener)
+
+        self.start()
+
+    def ownRoleManagerListener(self, msg):
+        # manage messages FROM RoleManager reading from its subscriber
+
+        # 0 (another Kid touched the color)
+        # 1 (number of total players)
+
+        if msg[0] == "0":
+            self.noWinners += 1
+        elif msg[0] == "1":
+            self.noTotKids = int(msg[2:])
+
+    def start(self):
+        rate_start = rospy.Rate(1)
+        rate_start.sleep()
+
+        while not(rospy.is_shutdown()):
+            # print("hey")
+            i = 1
+
     def listen(self, msg):
         #PS
         #TODO listen behaviour
+        i = 0
 
     def determineEndGame(self):
         #B
         #TODO determine end game behaviour
+        i = 0
 
     def communicateLoser(self):
         #MS
-        #TODO broadcast 
+        #TODO broadcast
+        i = 0
 
-    def config(self,*args): #TODO *args
+    def config(self,*args):
+        #TODO *args
         #TODO init function (if needed)
+        i = 0
 
     def mainLoop(self):
         #TODO mainLoop
+        i = 0
+
+
+if __name__ == "__main__":
+    rb = Witch()
+
