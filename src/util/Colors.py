5b76f698f10ba73970961ec318085066e5c6bf84
Andrea <andrea.poll93@gmail.com>
2019-03-05 11:42:16 +0100

    Color palette changed

--- src/util/Colors.py
+++ src/util/Colors.py
@@ -7,19 +7,15 @@ class Colors:
     colorNames = []
     kinectValues = []
 
-    colorNames.append('RED')
     colorNames.append('BLUE')
-    colorNames.append('GREEN')
-    colorNames.append('YELLOW')
     colorNames.append('MAGENTA')
     colorNames.append('LIGHT BLUE')
+    colorNames.append('LIGHT GREEN')
 
-    kinectValues.append((np.array([0,50,50]),np.array([15,255,255])))
-    kinectValues.append((np.array([105,50,50]),np.array([135,255,255])))
-    kinectValues.append((np.array([45,50,50]),np.array([75,255,255])))
-    kinectValues.append((np.array([15,50,50]),np.array([45,255,255])))
+    kinectValues.append((np.array([115,50,50]),np.array([125,255,255])))
     kinectValues.append((np.array([145,50,50]),np.array([160,255,255])))
     kinectValues.append((np.array([70, 50, 50]), np.array([110, 255, 255])))
+    kinectValues.append((np.array([38, 50, 50]), np.array([48, 255, 255])))
 
 def getColor(colorName):
     colorID = Colors.colorNames.index(colorName)
